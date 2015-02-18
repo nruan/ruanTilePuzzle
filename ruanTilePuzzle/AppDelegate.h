5b1e72b22d00992459c60893efe32bf14e2e6097
student <none@none.com>
2015-02-18 14:33:35 -0800

    Initial Commit

---
+++ ruanTilePuzzle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ruanTilePuzzle
+//
+//  Created by student on 2/18/15.
+//  Copyright (c) 2015 SSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ruanTilePuzzle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ruanTilePuzzle
+//
+//  Created by student on 2/18/15.
+//  Copyright (c) 2015 SSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
